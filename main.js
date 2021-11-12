44c0b3da677da568dbbf525dafe70a36aadeb46f
Samit Koyom <samitkoyom@gmail.com>
2021-11-13 02:15:44 +0700

    ch2 finished

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'iPhone 12',
+            description: 'New Apple A14 Bionic hight perfomance' 
+        }
+    }
+})
