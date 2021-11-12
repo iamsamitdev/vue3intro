cf520ddc34940f07a884e4aca74e29240663b0ad
Samit Koyom <samitkoyom@gmail.com>
2021-11-13 03:16:03 +0700

    finished ch5

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ const app = Vue.createApp({
             image: './assets/images/iphone-12-red.png',
             description:'New Apple A14 Bionic hight perfomance',
             inStock: true,
-            inventory: 8
+            inventory: 8,
+            details: ['64GB','128GB','256GB'], // collection
+            variants: [
+                {id: 239, color: 'Red', image: './assets/images/iphone-12-red.png'},
+                {id: 240, color: 'Purple', image: './assets/images/iphone-12-purple.png'},
+                {id: 250, color: 'White', image: './assets/images/iphone-12-white.png'},
+            ]
         }
     }
 })
